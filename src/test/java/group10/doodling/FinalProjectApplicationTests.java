c892df0e0452c943376afe12bc70db617a2c8038
JeongSH1 <jyt001532@gmail.com>
2024-05-23 15:40:11 +0900

    inital commit

---
+++ src/test/java/group10/doodling/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package group10.doodling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
