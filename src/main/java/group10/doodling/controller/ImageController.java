1ea1385aeccb75669ea7c6a57802d628c0ccaa03
JeongSH1 <jyt001532@gmail.com>
2024-05-28 21:12:52 +0900

    feat: image controller 구현

--- src/main/java/group10/doodling/controller/ImageController.java
+++ src/main/java/group10/doodling/controller/ImageController.java
@@ -1,7 +1,38 @@
 package group10.doodling.controller;
 
+import group10.doodling.component.ImageManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Controller
+@RequiredArgsConstructor
 public class ImageController {
+
+    private final ImageManager imageManager;
+
+    @ResponseBody
+    @GetMapping("api/image")
+    public ResponseEntity<Resource> downloadImage(@RequestParam String fileName) throws
+            IOException {
+        String fullPath = imageManager.getFullPath(fileName);
+        MediaType mediaType = MediaType.parseMediaType(Files.probeContentType(Paths.get(fullPath)));
+        UrlResource resource = new UrlResource("file:" + fullPath);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_TYPE, mediaType.toString())
+                .body(resource);
+    }
+
 }
--- src/main/java/group10/doodling/filter/AuthenticationFilter.java
+++ src/main/java/group10/doodling/filter/AuthenticationFilter.java
@@ -31,7 +31,8 @@ public class AuthenticationFilter extends OncePerRequestFilter {
             "/api/auth/login/oauth2/callback",
             "/api/test-exception",
             "/api/test-mongodb-user",
-            "/api/test-upload"
+            "/api/test-upload",
+            "/api/image"
     };
     private final TokenManager tokenManager;
     private final ObjectMapper objectMapper = new ObjectMapper();
