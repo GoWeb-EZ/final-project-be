201d7b5a8cf894dffbfafe7313fe7943c0559cbc
JeongSH1 <jyt001532@gmail.com>
2024-06-05 20:36:54 +0900

    refact: Oauth 호출 담당 Component 생성

--- src/main/java/group10/doodling/controller/AuthController.java
+++ src/main/java/group10/doodling/controller/AuthController.java
@@ -1,14 +1,13 @@
 package group10.doodling.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import group10.doodling.component.OAuthManager;
 import group10.doodling.component.TokenManager;
 import group10.doodling.controller.dto.request.auth.OauthUserInfoResponseDTO;
-import group10.doodling.controller.dto.request.auth.VerifyTokenRequestDTO;
 import group10.doodling.controller.dto.response.auth.LoginURLResponseDTO;
 import group10.doodling.controller.dto.response.auth.TokenResponseDTO;
 import group10.doodling.controller.dto.response.auth.VerifyTokenResponseDTO;
 import group10.doodling.service.AuthService;
-import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,10 +19,11 @@ public class AuthController {
 
     private final AuthService authService;
     private final TokenManager tokenManager;
+    private final OAuthManager oAuthManager;
 
     @GetMapping("/login-url")
     public ResponseEntity<LoginURLResponseDTO> getLoginURL() {
-        String loginURL = authService.createRedirectURI(AuthService.OauthType.KAKAO);
+        String loginURL = oAuthManager.createRedirectURI(OAuthManager.OauthType.KAKAO);
 
         LoginURLResponseDTO loginURLResponseDTO = new LoginURLResponseDTO();
         loginURLResponseDTO.setLoginURL(loginURL);
@@ -33,8 +33,8 @@ public class AuthController {
     @GetMapping("/login/oauth2/callback")
     public ResponseEntity<TokenResponseDTO> login(@RequestParam String code, @RequestParam("redirect_uri") String redirectURI) throws JsonProcessingException {
 
-        String accessToken = authService.getAccessToken(AuthService.OauthType.KAKAO, code, redirectURI);
-        OauthUserInfoResponseDTO oauthUserInfoResponseDTO = authService.getUserInfoByOauthAPI(AuthService.OauthType.KAKAO, accessToken);
+        String accessToken = oAuthManager.getAccessToken(OAuthManager.OauthType.KAKAO, code, redirectURI);
+        OauthUserInfoResponseDTO oauthUserInfoResponseDTO = oAuthManager.getUserInfoByOauthAPI(OAuthManager.OauthType.KAKAO, accessToken);
         String userId = authService.login(oauthUserInfoResponseDTO.getOauthId(), oauthUserInfoResponseDTO.getUsername());
         String token = tokenManager.generateToken(userId, oauthUserInfoResponseDTO.getUsername());
         TokenResponseDTO tokenResponseDTO = new TokenResponseDTO();
--- src/main/java/group10/doodling/service/AuthService.java
+++ src/main/java/group10/doodling/service/AuthService.java
@@ -3,6 +3,7 @@ package group10.doodling.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import group10.doodling.component.OAuthManager;
 import group10.doodling.controller.dto.request.auth.OauthUserInfoResponseDTO;
 import group10.doodling.entity.User;
 import group10.doodling.repository.UserRepository;
@@ -19,86 +20,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class AuthService {
 
-    @Value("${oauth.kakao-client-key}")
-    private String KAKAO_CLIENT_KEY;
-
-    @Value("${oauth.kakao-client-secret}")
-    private String KAKAO_CLIENT_SECRET;
-
-    @Value("${host-address}")
-    private String HOST_ADDRESS;
-
-    private final RestTemplate restTemplate = new RestTemplate();
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
     private final UserRepository userRepository;
-
-    public enum OauthType {
-        KAKAO
-    }
-
-    public String createRedirectURI(OauthType oauthType) {
-        if (oauthType.equals(OauthType.KAKAO))
-            return createKakaoRedirectURI();
-        return null;
-    }
-
-    private String createKakaoRedirectURI() {
-        return "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id="+KAKAO_CLIENT_KEY+"&redirect_uri=http://"+HOST_ADDRESS+":8080/api/auth/login/oauth2/callback";
-    }
-
-    public String getAccessToken(OauthType oauthType, String code, String redirectURI) throws JsonProcessingException {
-        if (oauthType.equals(OauthType.KAKAO))
-            return getKakaoOauthAccessToken(code, redirectURI);
-        return null;
-    }
-
-    private String getKakaoOauthAccessToken(String code, String redirectURI) throws JsonProcessingException {
-
-        String kakaoOauthTokenEndPoint = "https://kauth.kakao.com/oauth/token";
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/x-www-form-urlencoded;charset=utf-8");
-
-        MultiValueMap<String,String> body = new LinkedMultiValueMap<>();
-
-        body.add("grant_type","authorization_code");
-        body.add("client_id", KAKAO_CLIENT_KEY);
-        body.add("redirect_uri",redirectURI);
-        body.add("code",code);
-
-
-        ResponseEntity<String> oauthTokenResponse = restTemplate.postForEntity(kakaoOauthTokenEndPoint, new HttpEntity<>(body, headers), String.class);
-
-        JsonNode tokenResponseToJsonNode = objectMapper.readTree(oauthTokenResponse.getBody());
-        return tokenResponseToJsonNode.get("access_token").asText();
-    }
-
-    public OauthUserInfoResponseDTO getUserInfoByOauthAPI(OauthType oauthType, String accessToken) throws JsonProcessingException {
-        if (oauthType.equals(OauthType.KAKAO))
-            return getUserInfoByKakaoOauthAPI(accessToken);
-        return null;
-    }
-
-    private OauthUserInfoResponseDTO getUserInfoByKakaoOauthAPI(String accessToken) throws JsonProcessingException {
-
-        String kakaoOauthUserInfoAPIEndpoint = "https://kapi.kakao.com/v2/user/me";
-
-        ResponseEntity<String> userInfoResponse = restTemplate.exchange(kakaoOauthUserInfoAPIEndpoint, HttpMethod.GET, new HttpEntity<>(createAuthorizationHeader(accessToken)), String.class);
-
-        JsonNode jsonNode = objectMapper.readTree(userInfoResponse.getBody());
-        OauthUserInfoResponseDTO oauthUserInfoResponseDTO = new OauthUserInfoResponseDTO();
-        oauthUserInfoResponseDTO.setOauthId(jsonNode.get("id").asLong());
-        oauthUserInfoResponseDTO.setUsername(jsonNode.get("properties").get("nickname").asText());
-
-        return oauthUserInfoResponseDTO;
-    }
-
-    HttpHeaders createAuthorizationHeader(String token){
-        return new HttpHeaders() {{
-            set( "Authorization", "Bearer " + token );
-        }};
-    }
 
     public String login(Long oauthId, String name) {
         Optional<User> existingUser = userRepository.findByOauthId(oauthId);
