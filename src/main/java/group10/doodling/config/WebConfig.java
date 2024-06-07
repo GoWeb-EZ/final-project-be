f5af59ab7459599808bee483e0569b6f3a445c71
JeongSH1 <jyt001532@gmail.com>
2024-06-07 23:16:02 +0900

    fix: 허용 origin 수정

--- src/main/java/group10/doodling/config/WebConfig.java
+++ src/main/java/group10/doodling/config/WebConfig.java
@@ -22,8 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // 허용할 오리진 설정 (현재는 로컬에서만 테스트)
-                .allowedOrigins("https://final-project-fe-iota.vercel.app")
+                .allowedOrigins("http://localhost:3000", "https://final-project-fe-iota.vercel.app") // 허용할 오리진 설정 (현재는 로컬에서만 테스트)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드 설정
                 .allowedHeaders("*") // 허용할 헤더 설정, 별표(*)는 모든 헤더를 허용
                 .allowCredentials(true); // 모든 오리진을 허용하면 동작 오류, 특정 오리진을 설정해주어야 함.
